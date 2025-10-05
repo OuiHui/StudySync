b91345077e15b64dd59cfe5c8c1d30bb0eaab546
Huy Nguy <huy.nguyen77@icloud.com>
2025-10-05 17:41:15 -0400

    Remove caution notice

--- src/components/groups/StudyGroups.tsx
+++ src/components/groups/StudyGroups.tsx
@@ -429,27 +429,6 @@ export const StudyGroups = ({ onSelectGroup }: StudyGroupsProps) => {
               {loading ? 'Loading...' : `${filteredGroups.length} groups found`}
             </p>
           </div>
-
-          {/* Temporary notice about current limitations */}
-          {!isAnonymousUser() && (
-            <div className="bg-blue-50 dark:bg-blue-900/20 border border-blue-200 dark:border-blue-800 rounded-lg p-4 mb-6">
-              <div className="flex items-start">
-                <div className="flex-shrink-0">
-                  <svg className="w-5 h-5 text-blue-400" fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
-                  </svg>
-                </div>
-                <div className="ml-3">
-                  <h3 className="text-sm font-medium text-blue-800 dark:text-blue-200">
-                    Currently showing groups you created
-                  </h3>
-                  <div className="mt-1 text-sm text-blue-700 dark:text-blue-300">
-                    <p>Due to database configuration issues, only groups you've created are shown. Groups you've joined will be available once the database policies are updated.</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )}
           
           {loading ? (
             <div className="flex justify-center items-center py-20">
