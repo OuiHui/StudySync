8277fe582d990f36847928b28f785b9486e506ab
Huy Nguy <huy.nguyen77@icloud.com>
2025-10-05 18:07:26 -0400

    Update repository name from study-sphere-pomodoro-flow to StudySync

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/study-sphere-pomodoro-flow/' : '/',
+  base: mode === 'production' ? '/StudySync/' : '/',
   server: {
     host: "::",
     port: 8080,
